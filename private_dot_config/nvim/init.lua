ab6130b17c68e232f617b428e0ec95ff121530ed
Michael Forman <mforman@gmail.com>
2023-11-16 14:41:27 +0000

    Sensible starting point for using neovim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,21 +1,22 @@
+-- Leader key first
+-------------------------------------------------------------------------------
+vim.g.mapleader = " "
+
 -------------------------------------------------------------------------------
 -- Options
 -------------------------------------------------------------------------------
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.o.mouse = "a"
+
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir= os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -25,15 +26,7 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 
-vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "80"
-
--------------------------------------------------------------------------------
--- Keymap
--------------------------------------------------------------------------------
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -------------------------------------------------------------------------------
 -- Bootstrap Package Manager
@@ -55,46 +48,170 @@ vim.opt.rtp:prepend(lazypath)
 -- Plugins
 -------------------------------------------------------------------------------
 require("lazy").setup {
+  -- Color scheme
   { "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-	-- load the colorscheme here
-	vim.cmd([[colorscheme tokyonight]])
-      end,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight-storm]])
+    end,
   },
+
+  -- Vim Training Game
+  "ThePrimeagen/vim-be-good",
+
+  -- Git related plugin
+  "tpope/vim-fugitive",
+
+  -- Detect tabstop and shiftwidth automatically
+  "tpope/vim-sleuth",
+
+  -- Format the status line 
   { "nvim-lualine/lualine.nvim",
-        dependencies = "kyazdani42/nvim-web-devicons",
-        opts = { theme = "tokyonight" } },
+    dependencies = "kyazdani42/nvim-web-devicons",
+    opts = { theme = "tokyonight" } },
+
+  -- Hints about key bindings
   { "folke/which-key.nvim",
-      event = "VeryLazy",
-      init = function()
-          vim.o.timeout = true
-          vim.o.timeoutlen = 300
-      end,
-      opts = { }
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {}
   },
-  { "folke/neoconf.nvim", 
-      cmd = "Neoconf" },
+
+  -- Manage global and project-local settings
+  { "folke/neoconf.nvim",
+    cmd = "Neoconf" },
+
+  -- Completion help for init.lua and plugin development
   { "folke/neodev.nvim" },
-  { "nvim-telescope/telescope.nvim", 
-      tag = "0.1.4", 
-      dependencies = "nvim-lua/plenary.nvim",
-      keys = {
-          { "<leader>ff", "<CMD>Telescope find_files<CR>", mode = { "n" } },
-	  { "<leader>fg", "<CMD>Telescope git_files<CR>", mode = { "n" } },
-	  { "<leader>fs", "<CMD>Telescope live_grep<CR>", mode = { "n" } },
+
+  -- Cool fuzzy finder to jump to files
+  { "nvim-telescope/telescope.nvim",
+    tag = "0.1.4",
+    dependencies = "nvim-lua/plenary.nvim",
+  },
+
+  -- Syntax parsing
+  { "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = { "c", "lua", "python" },
+        auto_install = true,
+        highlight = { enable = true, }
       }
-   },
-   { "nvim-treesitter/nvim-treesitter",
-       build = ":TSUpdate",
-       config = function()
-            require("nvim-treesitter.configs").setup {
-                ensure_installed = { "c", "lua", "python" },
-                auto_install = true,
-		highlight = { enable = true, }
-            }
-        end },
-    { "ThePrimeagen/vim-be-good" }
+    end },
+
+  -- LSP
+  { 'williamboman/mason.nvim' },
+  { 'williamboman/mason-lspconfig.nvim' },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = true,
+    config = false,
+  },
+  -- LSP Support
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { 'hrsh7th/cmp-nvim-lsp' },
+    }
+  },
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      { 'L3MON4D3/LuaSnip' }
+    },
+  },
 }
 
+-------------------------------------------------------------------------------
+-- LSP Setup
+-------------------------------------------------------------------------------
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({ buffer = bufnr })
+  local opts = { buffer = bufnr }
+
+  vim.keymap.set({ 'n', 'x' }, 'gq', function()
+    vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+  end, opts)
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      -- (Optional) configure lua language server
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  }
+})
+
+-------------------------------------------------------------------------------
+-- Autocompletion config
+-------------------------------------------------------------------------------
+local cmp = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  })
+})
+
+-------------------------------------------------------------------------------
+-- Keymap with which-key so we have some descriptions
+-------------------------------------------------------------------------------
+local wk = require("which-key")
+
+wk.register({
+  p = {
+    name = "project",
+    v = { vim.cmd.Ex, "netrw directory listing" }
+  },
+}, { prefix = "<leader>" })
+
+wk.register({
+  f = {
+    name = "file",
+      f = { "<CMD>Telescope find_files<CR>", "Find File"},
+      g = { "<CMD>Telescope git_files<CR>", "Find Files (in git)"},
+      s = { "<CMD>Telescope live_grep<CR>", "Search in Files"},
+    },
+}, { prefix = "<leader>" })
+
+-------------------------------------------------------------------------------
+-- Other keymaps 
+-------------------------------------------------------------------------------
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
