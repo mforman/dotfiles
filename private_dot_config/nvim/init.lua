1c4b77f81665e24ec9a32e0403ad12d2439f0b60
Michael Forman <michael.forman@mintecglobal.com>
2024-03-02 10:33:19 +0000

    tweaks for a new windows machine

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -339,6 +339,10 @@ require('lazy').setup({
   },
 
   {
+    "jmederosalvarado/roslyn.nvim"
+  },
+
+  {
     'neovim/nvim-lspconfig',
     config = function()
       -- Switch for controlling whether you want autoformatting.
@@ -408,174 +412,174 @@ require('lazy').setup({
       })
     end,
   },
-  {
-    "robitx/gp.nvim",
-    config = function()
-      require("gp").setup()
-
-      -- or setup with your own config (see Install > Configuration in Readme)
-      -- require("gp").setup(config)
-
-      -- shortcuts might be setup here (see Usage > Shortcuts in Readme)
-      -- VISUAL mode mappings
-      -- s, x, v modes are handled the same way by which_key
-      require("which-key").register({
-        -- ...
-        ["<C-g>"] = {
-          c = { ":<C-u>'<,'>GpChatNew<cr>", "Visual Chat New" },
-          p = { ":<C-u>'<,'>GpChatPaste<cr>", "Visual Chat Paste" },
-          t = { ":<C-u>'<,'>GpChatToggle<cr>", "Visual Toggle Chat" },
-
-          ["<C-x>"] = { ":<C-u>'<,'>GpChatNew split<cr>", "Visual Chat New split" },
-          ["<C-v>"] = { ":<C-u>'<,'>GpChatNew vsplit<cr>", "Visual Chat New vsplit" },
-          ["<C-t>"] = { ":<C-u>'<,'>GpChatNew tabnew<cr>", "Visual Chat New tabnew" },
-
-          r = { ":<C-u>'<,'>GpRewrite<cr>", "Visual Rewrite" },
-          a = { ":<C-u>'<,'>GpAppend<cr>", "Visual Append (after)" },
-          b = { ":<C-u>'<,'>GpPrepend<cr>", "Visual Prepend (before)" },
-          i = { ":<C-u>'<,'>GpImplement<cr>", "Implement selection" },
-
-          g = {
-            name = "generate into new ..",
-            p = { ":<C-u>'<,'>GpPopup<cr>", "Visual Popup" },
-            e = { ":<C-u>'<,'>GpEnew<cr>", "Visual GpEnew" },
-            n = { ":<C-u>'<,'>GpNew<cr>", "Visual GpNew" },
-            v = { ":<C-u>'<,'>GpVnew<cr>", "Visual GpVnew" },
-            t = { ":<C-u>'<,'>GpTabnew<cr>", "Visual GpTabnew" },
-          },
-
-          n = { "<cmd>GpNextAgent<cr>", "Next Agent" },
-          s = { "<cmd>GpStop<cr>", "GpStop" },
-          x = { ":<C-u>'<,'>GpContext<cr>", "Visual GpContext" },
-
-          w = {
-            name = "Whisper",
-            w = { ":<C-u>'<,'>GpWhisper<cr>", "Whisper" },
-            r = { ":<C-u>'<,'>GpWhisperRewrite<cr>", "Whisper Rewrite" },
-            a = { ":<C-u>'<,'>GpWhisperAppend<cr>", "Whisper Append (after)" },
-            b = { ":<C-u>'<,'>GpWhisperPrepend<cr>", "Whisper Prepend (before)" },
-            p = { ":<C-u>'<,'>GpWhisperPopup<cr>", "Whisper Popup" },
-            e = { ":<C-u>'<,'>GpWhisperEnew<cr>", "Whisper Enew" },
-            n = { ":<C-u>'<,'>GpWhisperNew<cr>", "Whisper New" },
-            v = { ":<C-u>'<,'>GpWhisperVnew<cr>", "Whisper Vnew" },
-            t = { ":<C-u>'<,'>GpWhisperTabnew<cr>", "Whisper Tabnew" },
-          },
-        },
-        -- ...
-      }, {
-        mode = "v", -- VISUAL mode
-        prefix = "",
-        buffer = nil,
-        silent = true,
-        noremap = true,
-        nowait = true,
-      })
-
-      -- NORMAL mode mappings
-      require("which-key").register({
-        -- ...
-        ["<C-g>"] = {
-          c = { "<cmd>GpChatNew<cr>", "New Chat" },
-          t = { "<cmd>GpChatToggle<cr>", "Toggle Chat" },
-          f = { "<cmd>GpChatFinder<cr>", "Chat Finder" },
-
-          ["<C-x>"] = { "<cmd>GpChatNew split<cr>", "New Chat split" },
-          ["<C-v>"] = { "<cmd>GpChatNew vsplit<cr>", "New Chat vsplit" },
-          ["<C-t>"] = { "<cmd>GpChatNew tabnew<cr>", "New Chat tabnew" },
-
-          r = { "<cmd>GpRewrite<cr>", "Inline Rewrite" },
-          a = { "<cmd>GpAppend<cr>", "Append (after)" },
-          b = { "<cmd>GpPrepend<cr>", "Prepend (before)" },
-
-          g = {
-            name = "generate into new ..",
-            p = { "<cmd>GpPopup<cr>", "Popup" },
-            e = { "<cmd>GpEnew<cr>", "GpEnew" },
-            n = { "<cmd>GpNew<cr>", "GpNew" },
-            v = { "<cmd>GpVnew<cr>", "GpVnew" },
-            t = { "<cmd>GpTabnew<cr>", "GpTabnew" },
-          },
-
-          n = { "<cmd>GpNextAgent<cr>", "Next Agent" },
-          s = { "<cmd>GpStop<cr>", "GpStop" },
-          x = { "<cmd>GpContext<cr>", "Toggle GpContext" },
-
-          w = {
-            name = "Whisper",
-            w = { "<cmd>GpWhisper<cr>", "Whisper" },
-            r = { "<cmd>GpWhisperRewrite<cr>", "Whisper Inline Rewrite" },
-            a = { "<cmd>GpWhisperAppend<cr>", "Whisper Append (after)" },
-            b = { "<cmd>GpWhisperPrepend<cr>", "Whisper Prepend (before)" },
-            p = { "<cmd>GpWhisperPopup<cr>", "Whisper Popup" },
-            e = { "<cmd>GpWhisperEnew<cr>", "Whisper Enew" },
-            n = { "<cmd>GpWhisperNew<cr>", "Whisper New" },
-            v = { "<cmd>GpWhisperVnew<cr>", "Whisper Vnew" },
-            t = { "<cmd>GpWhisperTabnew<cr>", "Whisper Tabnew" },
-          },
-        },
-        -- ...
-      }, {
-        mode = "n", -- NORMAL mode
-        prefix = "",
-        buffer = nil,
-        silent = true,
-        noremap = true,
-        nowait = true,
-      })
-
-      -- INSERT mode mappings
-      require("which-key").register({
-        -- ...
-        ["<C-g>"] = {
-          c = { "<cmd>GpChatNew<cr>", "New Chat" },
-          t = { "<cmd>GpChatToggle<cr>", "Toggle Chat" },
-          f = { "<cmd>GpChatFinder<cr>", "Chat Finder" },
-
-          ["<C-x>"] = { "<cmd>GpChatNew split<cr>", "New Chat split" },
-          ["<C-v>"] = { "<cmd>GpChatNew vsplit<cr>", "New Chat vsplit" },
-          ["<C-t>"] = { "<cmd>GpChatNew tabnew<cr>", "New Chat tabnew" },
-
-          r = { "<cmd>GpRewrite<cr>", "Inline Rewrite" },
-          a = { "<cmd>GpAppend<cr>", "Append (after)" },
-          b = { "<cmd>GpPrepend<cr>", "Prepend (before)" },
-
-          g = {
-            name = "generate into new ..",
-            p = { "<cmd>GpPopup<cr>", "Popup" },
-            e = { "<cmd>GpEnew<cr>", "GpEnew" },
-            n = { "<cmd>GpNew<cr>", "GpNew" },
-            v = { "<cmd>GpVnew<cr>", "GpVnew" },
-            t = { "<cmd>GpTabnew<cr>", "GpTabnew" },
-          },
-
-          x = { "<cmd>GpContext<cr>", "Toggle GpContext" },
-          s = { "<cmd>GpStop<cr>", "GpStop" },
-          n = { "<cmd>GpNextAgent<cr>", "Next Agent" },
-
-          w = {
-            name = "Whisper",
-            w = { "<cmd>GpWhisper<cr>", "Whisper" },
-            r = { "<cmd>GpWhisperRewrite<cr>", "Whisper Inline Rewrite" },
-            a = { "<cmd>GpWhisperAppend<cr>", "Whisper Append (after)" },
-            b = { "<cmd>GpWhisperPrepend<cr>", "Whisper Prepend (before)" },
-            p = { "<cmd>GpWhisperPopup<cr>", "Whisper Popup" },
-            e = { "<cmd>GpWhisperEnew<cr>", "Whisper Enew" },
-            n = { "<cmd>GpWhisperNew<cr>", "Whisper New" },
-            v = { "<cmd>GpWhisperVnew<cr>", "Whisper Vnew" },
-            t = { "<cmd>GpWhisperTabnew<cr>", "Whisper Tabnew" },
-          },
-        },
-        -- ...
-      }, {
-        mode = "i", -- INSERT mode
-        prefix = "",
-        buffer = nil,
-        silent = true,
-        noremap = true,
-        nowait = true,
-      })
-    end,
-  },
+  -- {
+  --   "robitx/gp.nvim",
+  --   config = function()
+  --     require("gp").setup()
+  --
+  --     -- or setup with your own config (see Install > Configuration in Readme)
+  --     -- require("gp").setup(config)
+  --
+  --     -- shortcuts might be setup here (see Usage > Shortcuts in Readme)
+  --     -- VISUAL mode mappings
+  --     -- s, x, v modes are handled the same way by which_key
+  --     require("which-key").register({
+  --       -- ...
+  --       ["<C-g>"] = {
+  --         c = { ":<C-u>'<,'>GpChatNew<cr>", "Visual Chat New" },
+  --         p = { ":<C-u>'<,'>GpChatPaste<cr>", "Visual Chat Paste" },
+  --         t = { ":<C-u>'<,'>GpChatToggle<cr>", "Visual Toggle Chat" },
+  --
+  --         ["<C-x>"] = { ":<C-u>'<,'>GpChatNew split<cr>", "Visual Chat New split" },
+  --         ["<C-v>"] = { ":<C-u>'<,'>GpChatNew vsplit<cr>", "Visual Chat New vsplit" },
+  --         ["<C-t>"] = { ":<C-u>'<,'>GpChatNew tabnew<cr>", "Visual Chat New tabnew" },
+  --
+  --         r = { ":<C-u>'<,'>GpRewrite<cr>", "Visual Rewrite" },
+  --         a = { ":<C-u>'<,'>GpAppend<cr>", "Visual Append (after)" },
+  --         b = { ":<C-u>'<,'>GpPrepend<cr>", "Visual Prepend (before)" },
+  --         i = { ":<C-u>'<,'>GpImplement<cr>", "Implement selection" },
+  --
+  --         g = {
+  --           name = "generate into new ..",
+  --           p = { ":<C-u>'<,'>GpPopup<cr>", "Visual Popup" },
+  --           e = { ":<C-u>'<,'>GpEnew<cr>", "Visual GpEnew" },
+  --           n = { ":<C-u>'<,'>GpNew<cr>", "Visual GpNew" },
+  --           v = { ":<C-u>'<,'>GpVnew<cr>", "Visual GpVnew" },
+  --           t = { ":<C-u>'<,'>GpTabnew<cr>", "Visual GpTabnew" },
+  --         },
+  --
+  --         n = { "<cmd>GpNextAgent<cr>", "Next Agent" },
+  --         s = { "<cmd>GpStop<cr>", "GpStop" },
+  --         x = { ":<C-u>'<,'>GpContext<cr>", "Visual GpContext" },
+  --
+  --         w = {
+  --           name = "Whisper",
+  --           w = { ":<C-u>'<,'>GpWhisper<cr>", "Whisper" },
+  --           r = { ":<C-u>'<,'>GpWhisperRewrite<cr>", "Whisper Rewrite" },
+  --           a = { ":<C-u>'<,'>GpWhisperAppend<cr>", "Whisper Append (after)" },
+  --           b = { ":<C-u>'<,'>GpWhisperPrepend<cr>", "Whisper Prepend (before)" },
+  --           p = { ":<C-u>'<,'>GpWhisperPopup<cr>", "Whisper Popup" },
+  --           e = { ":<C-u>'<,'>GpWhisperEnew<cr>", "Whisper Enew" },
+  --           n = { ":<C-u>'<,'>GpWhisperNew<cr>", "Whisper New" },
+  --           v = { ":<C-u>'<,'>GpWhisperVnew<cr>", "Whisper Vnew" },
+  --           t = { ":<C-u>'<,'>GpWhisperTabnew<cr>", "Whisper Tabnew" },
+  --         },
+  --       },
+  --       -- ...
+  --     }, {
+  --       mode = "v", -- VISUAL mode
+  --       prefix = "",
+  --       buffer = nil,
+  --       silent = true,
+  --       noremap = true,
+  --       nowait = true,
+  --     })
+  --
+  --     -- NORMAL mode mappings
+  --     require("which-key").register({
+  --       -- ...
+  --       ["<C-g>"] = {
+  --         c = { "<cmd>GpChatNew<cr>", "New Chat" },
+  --         t = { "<cmd>GpChatToggle<cr>", "Toggle Chat" },
+  --         f = { "<cmd>GpChatFinder<cr>", "Chat Finder" },
+  --
+  --         ["<C-x>"] = { "<cmd>GpChatNew split<cr>", "New Chat split" },
+  --         ["<C-v>"] = { "<cmd>GpChatNew vsplit<cr>", "New Chat vsplit" },
+  --         ["<C-t>"] = { "<cmd>GpChatNew tabnew<cr>", "New Chat tabnew" },
+  --
+  --         r = { "<cmd>GpRewrite<cr>", "Inline Rewrite" },
+  --         a = { "<cmd>GpAppend<cr>", "Append (after)" },
+  --         b = { "<cmd>GpPrepend<cr>", "Prepend (before)" },
+  --
+  --         g = {
+  --           name = "generate into new ..",
+  --           p = { "<cmd>GpPopup<cr>", "Popup" },
+  --           e = { "<cmd>GpEnew<cr>", "GpEnew" },
+  --           n = { "<cmd>GpNew<cr>", "GpNew" },
+  --           v = { "<cmd>GpVnew<cr>", "GpVnew" },
+  --           t = { "<cmd>GpTabnew<cr>", "GpTabnew" },
+  --         },
+  --
+  --         n = { "<cmd>GpNextAgent<cr>", "Next Agent" },
+  --         s = { "<cmd>GpStop<cr>", "GpStop" },
+  --         x = { "<cmd>GpContext<cr>", "Toggle GpContext" },
+  --
+  --         w = {
+  --           name = "Whisper",
+  --           w = { "<cmd>GpWhisper<cr>", "Whisper" },
+  --           r = { "<cmd>GpWhisperRewrite<cr>", "Whisper Inline Rewrite" },
+  --           a = { "<cmd>GpWhisperAppend<cr>", "Whisper Append (after)" },
+  --           b = { "<cmd>GpWhisperPrepend<cr>", "Whisper Prepend (before)" },
+  --           p = { "<cmd>GpWhisperPopup<cr>", "Whisper Popup" },
+  --           e = { "<cmd>GpWhisperEnew<cr>", "Whisper Enew" },
+  --           n = { "<cmd>GpWhisperNew<cr>", "Whisper New" },
+  --           v = { "<cmd>GpWhisperVnew<cr>", "Whisper Vnew" },
+  --           t = { "<cmd>GpWhisperTabnew<cr>", "Whisper Tabnew" },
+  --         },
+  --       },
+  --       -- ...
+  --     }, {
+  --       mode = "n", -- NORMAL mode
+  --       prefix = "",
+  --       buffer = nil,
+  --       silent = true,
+  --       noremap = true,
+  --       nowait = true,
+  --     })
+  --
+  --     -- INSERT mode mappings
+  --     require("which-key").register({
+  --       -- ...
+  --       ["<C-g>"] = {
+  --         c = { "<cmd>GpChatNew<cr>", "New Chat" },
+  --         t = { "<cmd>GpChatToggle<cr>", "Toggle Chat" },
+  --         f = { "<cmd>GpChatFinder<cr>", "Chat Finder" },
+  --
+  --         ["<C-x>"] = { "<cmd>GpChatNew split<cr>", "New Chat split" },
+  --         ["<C-v>"] = { "<cmd>GpChatNew vsplit<cr>", "New Chat vsplit" },
+  --         ["<C-t>"] = { "<cmd>GpChatNew tabnew<cr>", "New Chat tabnew" },
+  --
+  --         r = { "<cmd>GpRewrite<cr>", "Inline Rewrite" },
+  --         a = { "<cmd>GpAppend<cr>", "Append (after)" },
+  --         b = { "<cmd>GpPrepend<cr>", "Prepend (before)" },
+  --
+  --         g = {
+  --           name = "generate into new ..",
+  --           p = { "<cmd>GpPopup<cr>", "Popup" },
+  --           e = { "<cmd>GpEnew<cr>", "GpEnew" },
+  --           n = { "<cmd>GpNew<cr>", "GpNew" },
+  --           v = { "<cmd>GpVnew<cr>", "GpVnew" },
+  --           t = { "<cmd>GpTabnew<cr>", "GpTabnew" },
+  --         },
+  --
+  --         x = { "<cmd>GpContext<cr>", "Toggle GpContext" },
+  --         s = { "<cmd>GpStop<cr>", "GpStop" },
+  --         n = { "<cmd>GpNextAgent<cr>", "Next Agent" },
+  --
+  --         w = {
+  --           name = "Whisper",
+  --           w = { "<cmd>GpWhisper<cr>", "Whisper" },
+  --           r = { "<cmd>GpWhisperRewrite<cr>", "Whisper Inline Rewrite" },
+  --           a = { "<cmd>GpWhisperAppend<cr>", "Whisper Append (after)" },
+  --           b = { "<cmd>GpWhisperPrepend<cr>", "Whisper Prepend (before)" },
+  --           p = { "<cmd>GpWhisperPopup<cr>", "Whisper Popup" },
+  --           e = { "<cmd>GpWhisperEnew<cr>", "Whisper Enew" },
+  --           n = { "<cmd>GpWhisperNew<cr>", "Whisper New" },
+  --           v = { "<cmd>GpWhisperVnew<cr>", "Whisper Vnew" },
+  --           t = { "<cmd>GpWhisperTabnew<cr>", "Whisper Tabnew" },
+  --         },
+  --       },
+  --       -- ...
+  --     }, {
+  --       mode = "i", -- INSERT mode
+  --       prefix = "",
+  --       buffer = nil,
+  --       silent = true,
+  --       noremap = true,
+  --       nowait = true,
+  --     })
+  --   end,
+  -- },
   {
     'xvzc/chezmoi.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
@@ -614,7 +618,7 @@ vim.o.expandtab = true
 
 vim.o.smartindent = true
 
-vim.o.wrap = 0
+vim.o.wrap = false
 
 -- Hide the mode since it's in lualine
 vim.o.showmode = false
@@ -1006,9 +1010,9 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  clangd = {},
+  -- clangd = {},
   eslint = { packageManager = 'npm', },
-  gopls = {},
+  -- gopls = {},
   html = {
     filetypes = { 'html', 'twig', 'hbs' },
     html = {
@@ -1025,10 +1029,10 @@ local servers = {
     },
   },
   marksman = {},
-  omnisharp = {},
+  -- omnisharp = {},
   pyright = {},
   tsserver = {},
-  volar = {},
+  -- volar = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -1063,6 +1067,11 @@ mason_lspconfig.setup_handlers {
     }
   end,
 }
+
+require("roslyn").setup({
+  on_attach = on_attach,
+  capabilities = capabilities
+})
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
