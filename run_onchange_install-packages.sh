6e1073f5ac05b2e5f04bcc71c18cfd366b5583a4
Michael Forman <mforman@gmail.com>
2024-08-20 12:07:35 +0100

    Add additional linux packages

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -16,3 +16,15 @@ function install {
 # Basics
 install fzf
 install tmux
+
+curl -sS https://starship.rs/install.sh | sh
+curl https://pyenv.run | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+
