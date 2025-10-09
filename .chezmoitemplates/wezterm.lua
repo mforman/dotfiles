c6036443859d33ed60b962384b852482e9bddc40
Michael Forman <mforman@gmail.com>
2025-10-09 20:42:28 +0100

    update zshrc

--- .chezmoitemplates/wezterm.lua
+++ .chezmoitemplates/wezterm.lua
@@ -1,23 +1,22 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
-
 
 -- This table will hold the configuration
 local config = {}
 config.launch_menu = {}
 
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-	config.default_domain = 'WSL:Ubuntu'
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  config.default_domain = "WSL:Ubuntu"
   table.insert(config.launch_menu, {
-    label = 'New tab - WSL',
-    domain = { DomainName = 'WSL:Ubuntu' },
+    label = "New tab - WSL",
+    domain = { DomainName = "WSL:Ubuntu" },
   })
-	table.insert(config.launch_menu, {
-		label = 'New tab - PowerShell',
-		domain = { DomainName = 'local' },
-		args = { 'pwsh' },
-	})
+  table.insert(config.launch_menu, {
+    label = "New tab - PowerShell",
+    domain = { DomainName = "local" },
+    args = { "pwsh" },
+  })
 end
 
 config.automatically_reload_config = true
@@ -30,7 +29,7 @@ config.font_size = 16.5
 {{ else }}
 config.font_size = 12
 {{ end }}
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'Catppuccin Macchiato'
 config.default_cursor_style = 'SteadyBlock'
 
 config.inactive_pane_hsb = {
@@ -43,22 +42,28 @@ config.warn_about_missing_glyphs = false
 
 local use_native_mux = true
 if use_native_mux then
-	require("config.keys").apply(config)
+  require("config.keys").apply(config)
 
-	-- Replicate the status line in the tab bar
-	require("config.bar").apply_to_config(config, {
-		dividers = "rounded", -- "slant_right", "slant_left", "arrows", "rounded", false
-		tabs = {
-			pane_count = "icon"
-		},
-		clock = {
-			enabled = true,
-			format = " %Y-%m-%d %H:%M "
-		}
-	})
-	config.hide_tab_bar_if_only_one_tab = false
+  -- Replicate the status line in the tab bar
+  require("config.bar").apply_to_config(config, {
+    dividers = "rounded", -- "slant_right", "slant_left", "arrows", "rounded", false
+    tabs = {
+      pane_count = "icon",
+    },
+    clock = {
+      enabled = true,
+      format = " %Y-%m-%d %H:%M ",
+    },
+  })
+  config.hide_tab_bar_if_only_one_tab = false
+
+  wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
+    local workspace = wezterm.mux.get_active_workspace()
+    local title = tab.active_pane.title
+    return string.format("%s — %s", workspace or "WezTerm", title)
+  end)
 else
-	config.hide_tab_bar_if_only_one_tab = true
+  config.hide_tab_bar_if_only_one_tab = true
 end
 
 -- and finally, return the configuration to wezterm
