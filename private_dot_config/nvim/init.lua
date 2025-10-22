b64803063a3104f02040a9d0f717c9699e57c211
Michael Forman <mforman@gmail.com>
2025-10-22 12:46:49 +0100

    background

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -851,7 +851,9 @@ require('lazy').setup({
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('catppuccin').setup {}
+      require('catppuccin').setup {
+        transparent_background = true,
+      }
 
       -- Load the colorscheme here.
       vim.cmd.colorscheme 'catppuccin-macchiato'
